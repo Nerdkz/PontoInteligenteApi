829a7accef2275fb3d1221150c45bd5799258470
Vinicius Mendonça <viniciusmendonca@gec.inatel.br>
2019-03-21 09:05:44 -0300

    Testando..123

--- src/main/java/com/leucotron/pontointeligente/JApplication.java
+++ src/main/java/com/leucotron/pontointeligente/JApplication.java
@@ -8,6 +8,7 @@ public class JApplication {
 
 	public static void main(String[] args) {
                 System.out.println("Hello, World!!");
+                System.out.println("HUE br");
 	}
 
 }
