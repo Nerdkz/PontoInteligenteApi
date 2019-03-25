bc1cfd3639220111bfed6516cb3153bced63f7f5
Vinicius Mendonça <viniciusmendonca@gec.inatel.br>
2019-03-25 11:18:02 -0300

    Classes e Interfaces de tests para testes unitários Services/ ServicesImpl/ utils.

---
+++ src/main/java/com/leucotron/pontointeligente/impl/EmpresaServiceImpl.java
@@ -0,0 +1,34 @@
+package com.leucotron.pontointeligente.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leucotron.pontointeligente.api.entities.Empresa;
+import com.leucotron.pontointeligente.repositories.EmpresaRepository;
+import com.leucotron.pontointeligente.services.EmpresaService;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+    private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+
+    @Autowired
+    private EmpresaRepository empresaRepository;
+
+    @Override
+    public Optional<Empresa> buscarPorCnpj(String cnpj) {
+        log.info("Buscando uma empresa para o CNPJ {}", cnpj);
+        return Optional.ofNullable(empresaRepository.findByCnpj(cnpj));
+    }
+
+    @Override
+    public Empresa persistir(Empresa empresa) {
+        log.info("Persistindo empresa: {}", empresa);
+        return this.empresaRepository.save(empresa);
+    }
+
+}
